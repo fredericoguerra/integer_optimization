99cc90328d6288621607f4ded14b30f4c2b5fda1
FGUER <frederico.souza.guerra@gmail.com>
2022-06-16 22:19:12 -0300

    final model deployed

---
+++ README.md
@@ -0,0 +1,37 @@
+## Descrição do Problema
+
+Uma empresa de pesquisa em biotecnologia aplicada na agricultura planta diferentes populações genéticas de um determinada cultivar objetivando desenvolver os melhores produtos para o agricultor. Para isso, durante duas safras ao ano o time de planejamento precisa tomar a decisão de quantas unidades de área plantar a fim de garantir que, durante o processo, os times operacionais tenham plantas suficientes no campo para cultivar e alcançar o número de sementes necessário ($T_{i}$) para produção de cada material ($A_{i}$).
+
+No entanto, após uma análise detalhada dos dados históricos, a equipe encontrou uma alta taxa de excesso de sementes produzidas - o que indica uma utilização ineficiente dos recursos (pessoas, água, fertilizantes, área, sementes, etc.) necessários durante cada cultivo. Nesse cenário, o presente trabalho propõe um modelo de otimização em programação inteira a fim de minimizar a área planejada de plantio para um conjunto de 150 materiais e dadas alguns restrições requeridas pelo negócio.
+
+O arquivo *predictions.csv* é a entrada para o modelo proposto, *results.csv* contém as saídas do modelo desenvolvido utilizando a biblioteca CPLEX e o *final_recommendations.csv* apresenta a prescrição final de unidades de área de plantio por cada material.
+
+## Modelagem
+
+### Variáveis de decisão:
+
+$A_{i}$: Quantidade de unidades de área plantada do material $i$.
+
+$y_{i}$: Variável binária indicando se o material $i$ atingiu o volume de sementes esperado.
+
+$T_{i}$: Volume de sementes esperado para o material $i$
+
+$P_{i}$: Volume de sementes predito
+
+### Restrições:
+
+$ \frac{\sum_{i=0}^{n} y_{i}}{n} \ge 0.96$
+
+$ 1.23 * A_{i} * P_{i} \ge 2.5*T_{i}$
+
+$1.23 * A_{i} * P_{i} \le 2.5*T_{i} + 50000$
+
+$A_{i} \ge 0$
+
+$A_{i} \le 50$
+
+$y_{i} \in \{0,1\}$
+
+### Função Objetivo:
+
+$ Min f(A) = 1.23 * \sum_{i=0}^{n} A_{i}$
---
+++ modelo.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import numpy as np
+from docplex.mp.model import Model
+import matplotlib.pyplot as plt
+import json
+from numpy.ma.core import var
+
+predictions = pd.read_csv('predictions.csv',delimiter=';')
+predictions = predictions.head(150)
+predictions['seed_target_class'] = np.where(predictions['seed_target'] > 15000, 2, 1)
+
+
+
+margin = 2.5
+target = 97
+
+df_solution = pd.DataFrame()
+mdl = Model('reduction_factor')
+
+var_indices = predictions.index.values.tolist()
+num_plots_optimized_half = mdl.integer_var_dict(var_indices, name='NumPlotsOptimizedHalf')
+num_plots_optimized = mdl.integer_var_dict(var_indices, name = 'NumPlotsOptimized')
+seed_production_optimized = mdl.continuous_var_dict(var_indices, name='SeedProductionOptimized')
+metst_optimized = mdl.binary_var_dict(var_indices, name='MetstOptimized')
+
+M = 50000
+
+mdl.add_constraints(num_plots_optimized_half[i] <= 25 for i in var_indices)
+mdl.add_constraints(num_plots_optimized_half[i] >= 0 for i in var_indices)
+mdl.add_constraints(num_plots_optimized[i] == 2*num_plots_optimized_half[i] for i in var_indices)##
+mdl.add_constraints(seed_production_optimized[i] == 1.23 * num_plots_optimized[i] * predictions.loc[i, 'y_predicted'] for i in var_indices)##
+mdl.add_constraints(seed_production_optimized[i] <= predictions.loc[i,'seed_target']*margin + metst_optimized[i]*M for i in var_indices)##
+mdl.add_constraints(seed_production_optimized[i] >= predictions.loc[i,'seed_target']*margin for i in var_indices)##
+mdl.add_constraint(mdl.sum(metst_optimized[i] for i in var_indices)/len(predictions)*100 >= target)##
+
+area_planted = 1.23*(mdl.sum(num_plots_optimized[i] for i in var_indices)) ##
+hybrids_met_target = (mdl.sum(metst_optimized[i] for i in var_indices)/len(var_indices)*100)
+
+mdl.minimize(area_planted)
+mdl.solve()
+mdl.solution.export('results.json')
+
+
+
+df_solution = pd.DataFrame()
+with open('./results.json','r') as results:
+    data = json.load(results)
+    for i in data['CPLEXSolution']['variables']:
+        df_solution = df_solution.append(i,ignore_index=True)
+
+df_solution = df_solution.loc[df_solution['name'].str.find('factor_')==-1]
+df_solution['hyb_index'] = df_solution['name'].str.split('_',expand=True)[1]
+df_solution['parameter'] = df_solution['name'].str.split('_',expand=True)[0]
+df_pivot = df_solution.pivot(index="hyb_index", columns="parameter", values="value")
+df_pivot = df_pivot.fillna(0)
+df_pivot = df_pivot.reset_index()
+df_pivot['hyb_index'] = df_pivot['hyb_index'].astype(int)
+df_pivot.to_csv('results.csv')
+recommendation = pd.merge(how='left', left=predictions, right=df_pivot[['hyb_index','MetstOptimized','NumPlotsOptimized','NumPlotsOptimizedHalf','SeedProductionOptimized']], on='hyb_index', )
+recommendation['Recomendacao_Planejamento'] = 24
+recommendation['SeedProductionOptimized'] = np.round(recommendation['SeedProductionOptimized'].astype(float),0)
+recommendation = recommendation.fillna(0)
+recommendation['SeedProductionOptimized'] = recommendation['SeedProductionOptimized'].astype(int)
+recommendation['NumPlotsOptimized'] = recommendation['NumPlotsOptimized'].astype(float)
+recommendation.to_csv('final_recommendation.csv')
+
+print(f"Total de área recomendada pelo time de planejamento: {sum(recommendation['Recomendacao_Planejamento'])} \nTotal de área recomendada pelo modelo:{sum(recommendation['NumPlotsOptimized'])} \nUnidades de área relativa reduzida: {np.round((sum(recommendation['Recomendacao_Planejamento'])-sum(recommendation['NumPlotsOptimized']))/sum(recommendation['Recomendacao_Planejamento'])*100,2)}%")
